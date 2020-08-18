08324b225e4cf9890c5976160c3f99dc6960dc9e
Abdur Rehman <32234671+Arehman52@users.noreply.github.com>
2020-08-18 19:32:18 +0500

    17th aug updates

--- GBCL/src/app/app.module.ts
+++ GBCL/src/app/app.module.ts
@@ -7,12 +7,12 @@ import { STUDENTComponent } from './student/student.component';
 import { HomepageNavSearchComponent } from './gbcl-homepage/homepage-nav-search/homepage-nav-search.component';
 import { AboutusComponent } from './gbcl-homepage/aboutus/aboutus.component';
 import { SignupFormComponent } from './gbcl-homepage/signup-form/signup-form.component';
-import { AdminNavComponent } from './ADMIN/admin-nav/admin-nav.component';
 import { AdminComponent } from './admin/admin.component';
 import { GBCLHomepageComponent } from './gbcl-homepage/gbcl-homepage.component';
 import { TEACHERComponent } from './teacher/teacher.component';
 import { UNIVERSITYComponent } from './university/university.component';
 import { Error404Component } from './MISC/error404/error404.component';
+import { AdminNavComponent } from './admin/admin-nav/admin-nav.component';
 
 
 @NgModule({
@@ -22,12 +22,12 @@ import { Error404Component } from './MISC/error404/error404.component';
     HomepageNavSearchComponent,
     AboutusComponent,
     SignupFormComponent,
-    AdminNavComponent,
     AdminComponent,
     GBCLHomepageComponent,
     TEACHERComponent,
     UNIVERSITYComponent,
-    Error404Component
+    Error404Component,
+    AdminNavComponent,
   ],
   imports: [
     BrowserModule,
