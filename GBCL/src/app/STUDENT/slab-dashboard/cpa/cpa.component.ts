62213556409e63802b00b9ba36029b56f2baf409
Abdur Rehman <32234671+Arehman52@users.noreply.github.com>
2020-10-13 22:37:14 +0500

    Oct 13

--- GBCL/src/app/STUDENT/slab-dashboard/cpa/cpa.component.ts
+++ GBCL/src/app/STUDENT/slab-dashboard/cpa/cpa.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cpa',
   templateUrl: './cpa.component.html',
-  styleUrls: ['./cpa.component.css']
+  styleUrls: ['./cpa.component.css'],
 })
-export class CpaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CpaComponent {
+  members = [
+    {
+      StudentID: 's1',
+      status: 'online',
+    }
+  ];
+  tasks = [
+    {
+      taskID: '1',
+      taskTitle: 'Task 01',
+      XPsAllocated: '05',
+      taskContent: 'This is the content of task 1.',
+    },
+    {
+      taskID: '2',
+      taskTitle: 'Task 02',
+      XPsAllocated: '10',
+      taskContent: 'This is the content of task 2.',
+    },
+    {
+      taskID: '3',
+      taskTitle: 'Task 03, Upload the CV in JPEG format in the portal.',
+      XPsAllocated: '05',
+      taskContent: 'This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.This is the content of task 3.',
+    },
+    {
+      taskID: '4',
+      taskTitle: 'Task 04',
+      XPsAllocated: '25',
+      taskContent: 'This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.This is the content of task 4.',
+    },
+  ];
 }
